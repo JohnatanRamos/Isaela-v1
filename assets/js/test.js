4c18da7317bda0f8c6a9d38d14f7280c05147245
Jramos <jramos@saludelectronica.com>
2022-11-07 22:55:17 -0500

    Auto-generated commit [ci skip]

--- assets/js/test.js
+++ assets/js/test.js
@@ -4,9 +4,9 @@ var handler = ePayco.checkout.configure({
 });
 var data = {
   //Parametros compra (obligatorio)
-  name: "Pijama",
-  description: "Pijama",
-  invoice: "12734",
+  name: "Pijamas",
+  description: "Pijamas",
+  invoice: "127332224",
   currency: "cop",
   amount: "12000",
   tax_base: "0",
@@ -21,16 +21,15 @@ var data = {
   extra1: "extra1",
   extra2: "extra2",
   extra3: "extra3",
-  confirmation: "file:///C:/Users/Administrador/Documents/personal/proyectos/isaela/index.html",
-  response: "file:///C:/Users/Administrador/Documents/personal/proyectos/isaela/index.html",
-
+  confirmation: "http://localhost:4200/response",
+  response: "http://localhost:4200/response",
 
   //Atributos cliente
   name_billing: "Andres Perez",
   address_billing: "Carrera 19 numero 14 91",
   type_doc_billing: "cc",
   mobilephone_billing: "3050000000",
-  number_doc_billing: "100000000",
+  number_doc_billing: "0000000",
 
   //atributo deshabilitación metodo de pago
   methodsDisable: [],
